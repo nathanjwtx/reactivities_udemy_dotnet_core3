e92108d3bdcee09f52d006d2bb63244f7e475eca
Nathan <nathanjwtx@nathanjwtx.io>
2020-01-26 10:34:54 -0600

    Updated routes and methods

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -5,7 +5,7 @@ using Domain;
 using Application.Activities;
 using System.Collections.Generic;
 using System;
-using System.Threading;
+using Microsoft.Extensions.Logging;
 
 namespace API.Controllers
 {
@@ -14,27 +14,29 @@ namespace API.Controllers
     public class ActivitiesController : ControllerBase
     {
         private readonly IMediator _mediator;
+        private ILogger<ActivitiesController> _logger;
 
-        public ActivitiesController(IMediator mediator)
+        public ActivitiesController(IMediator mediator, ILogger<ActivitiesController> logger)
         {
+            this._logger = logger;
             _mediator = mediator;
         }
 
         [HttpGet]
-        public async Task<ActionResult<List<Activity>>> List(CancellationToken cancellationToken)
+        public async Task<ActionResult<List<Activity>>> List()
         {
             // List.Query comes from Applications.Activities
-            return await _mediator.Send(new List.Query(), cancellationToken).ConfigureAwait(false);
+            return await _mediator.Send(new List.Query()).ConfigureAwait(false);
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Activity>> Details(Guid id)
         {
-            return await _mediator.Send(new Details.Query{ Id = id }).ConfigureAwait(false);
+            return await _mediator.Send(new Details.Query { Id = id }).ConfigureAwait(false);
         }
 
         [HttpPost]
-        // FromBody isn't required as [ApiController] is in place. Added for clarification
+        // [FromBody] isn't required as [ApiController] is in place. Added for clarification
         public async Task<ActionResult<Unit>> Create([FromBody] Create.Command command)
         {
             return await _mediator.Send(command).ConfigureAwait(false);
@@ -43,6 +45,8 @@ namespace API.Controllers
         [HttpPut("{id}")]
         public async Task<ActionResult<Unit>> Edit(Guid id, Edit.Command command)
         {
+            // _logger.LogInformation($"id: {id}");
+            // _logger.LogInformation($"command: {command.Venue}");
             command.Id = id;
             return await _mediator.Send(command).ConfigureAwait(false);
         }
@@ -50,7 +54,7 @@ namespace API.Controllers
         [HttpDelete("{id}")]
         public async Task<ActionResult<Unit>> Delete(Guid id)
         {
-            return await _mediator.Send(new Delete.Command{Id = id}).ConfigureAwait(false);
+            return await _mediator.Send(new Delete.Command { Id = id }).ConfigureAwait(false);
         }
     }
 }
